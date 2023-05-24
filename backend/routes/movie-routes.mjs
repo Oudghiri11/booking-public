785b08c47102e3b51d9b6ade5c2f0ee997704d31
ahmed <ahmedoudrhiri12@gmail.com>
2023-05-24 21:55:48 +0200

    mjs

--- backend/routes/movie-routes.mjs
+++ backend/routes/movie-routes.mjs
@@ -4,7 +4,7 @@ import {
   getAllMovies,
   getMovieById,
   deleteMovieById,
-} from "../controllers/movie-controller.msj";
+} from "../controllers/movie-controller.mjs";
 const movieRouter = express.Router();
 movieRouter.get("/", getAllMovies);
 movieRouter.get("/:id", getMovieById);
